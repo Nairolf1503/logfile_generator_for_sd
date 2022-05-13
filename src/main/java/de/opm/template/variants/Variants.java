ce611ea048108d39b8b3e47cedb389757e81fcf9
Nairolf1503 <florian.rendschmidt@t-online.de>
2022-05-13 14:13:38 +0200

    variant creation called parallel

--- src/main/java/de/opm/template/variants/Variants.java
+++ src/main/java/de/opm/template/variants/Variants.java
@@ -1,7 +1,7 @@
 package de.opm.template.variants;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 import org.json.JSONObject;
 
@@ -46,5 +46,29 @@ public class Variants {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println(System.currentTimeMillis());
+
+        Set<String> key_set = json.keySet();
+        String[] keys = key_set.toArray(new String[key_set.size()]);
+
+        Thread[] creation_threads = new VariantFactoryThread[keys.length];
+
+        for(int i = 0 ; i < keys.length ; i++){
+            String variant_key = keys[i];
+            JSONObject variant_json = json.getJSONObject(variant_key);
+            creation_threads[i] = new VariantFactoryThread(variant_key, variant_json);
+            creation_threads[i].start();
+        }
+
+        for(int i = 0 ; i < creation_threads.length ; i++){
+            try {
+                creation_threads[i].join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis());
     }
 }
